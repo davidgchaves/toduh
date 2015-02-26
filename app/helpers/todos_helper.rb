38d6f5bdd3df758b0efc6881807635c49e09cde7
David G Chaves <davidgchaves@gmail.com>
2015-02-26 13:27:26 +0100

    Extract render_completion_button as a view helper for todos

---
+++ app/helpers/todos_helper.rb
@@ -0,0 +1,9 @@
+module TodosHelper
+  def render_completion_button(todo)
+    if todo.completed?
+      button_to "Mark incomplete", todo_completion_path(todo), method: :delete
+    else
+      button_to "Mark complete", todo_completion_path(todo)
+    end
+  end
+end
